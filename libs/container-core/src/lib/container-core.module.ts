26709cbc2dd44ab2c1928e60df9dd4b909a2566e
Michael <mikemountain.dev@gmail.com>
2022-03-15 17:50:57 +0200

    feature/layout (WIP) Begin blocking out the basic layout

--- libs/container-core/src/lib/container-core.module.ts
+++ libs/container-core/src/lib/container-core.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
+  declarations: [
+    LayoutComponent
+  ],
+  exports: [LayoutComponent]
 })
 export class ContainerCoreModule {}
