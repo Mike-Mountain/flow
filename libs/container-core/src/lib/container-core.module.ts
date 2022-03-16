5a06137ec92d3fd0d1fd6bae777d4a761a2aa860
Michael <mikemountain.dev@gmail.com>
2022-03-16 17:18:59 +0200

    Craete the layout

--- libs/container-core/src/lib/container-core.module.ts
+++ libs/container-core/src/lib/container-core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import {RouterModule} from "@angular/router";
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    NavigationComponent
   ],
   exports: [LayoutComponent]
 })
