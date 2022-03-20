38ab8eedfeaeda55a8bbe1669ef83aed5212b6bc
Michael <mikemountain.dev@gmail.com>
2022-03-20 14:09:32 +0200

    Set up horizontal scrolling and drag to scroll on the carousel

--- libs/shared/src/lib/directives/carousel.directive.ts
+++ libs/shared/src/lib/directives/carousel.directive.ts
@@ -1,33 +1,93 @@
 import {
   AfterViewInit,
   Directive,
-  ElementRef,
+  ElementRef, EventEmitter,
   HostListener,
-  Input,
-  OnChanges,
+  Input, Output,
   Renderer2,
-  SimpleChanges
 } from '@angular/core';
+
+
+interface position {
+  x: number;
+  y: number;
+  top: number;
+  left: number;
+}
+
+interface direction {
+  x: number;
+  y: number;
+}
 
 @Directive({
   selector: '[sharedCarousel]',
 })
 export class CarouselDirective implements AfterViewInit {
 
-  @Input() set imageIdx(value: number) {
-    if (value !== undefined) {
-      this.scrollTo(value);
-    }
+  @Input() set imageIdx(value: number | undefined) {
+    this._imageIdx = value;
   };
+
+  get imageIdx(): number | undefined {
+    return this._imageIdx;
+  }
+
+  @Output() selectImage = new EventEmitter<number>();
 
   private element: HTMLElement;
   private selectedImage: Element | undefined;
   private nodes: Element[] = [];
   private currentImageIdx = 0;
 
-  @HostListener('scroll', ['$event'])
-  handleClick(event: Event) {
-    // console.log(event);
+  private position: position | undefined;
+  private isScrolling = false;
+
+  private _imageIdx: number | undefined;
+
+  @HostListener('mousedown', ['$event'])
+  handleMouseDown(event: MouseEvent) {
+    this.isScrolling = false;
+    // Set up the position for dragging
+    this.position = {
+      x: event.clientX,
+      y: event.clientY,
+      top: this.element.scrollTop,
+      left: this.element.scrollLeft
+    }
+  }
+
+  @HostListener('click')
+  onImageClick() {
+    if (!this.isScrolling && this.imageIdx !== undefined) {
+      this.selectImage.emit(this.imageIdx);
+      this.scrollTo(this.imageIdx);
+    }
+  }
+
+  @HostListener('mousemove', ['$event'])
+  handleMouseMove(event: MouseEvent) {
+    // Scroll the container when the user drags their mouse
+    event.preventDefault();
+    if (this.position) {
+      this.isScrolling = true;
+      const direction: direction = {
+        x: event.clientX - this.position.x,
+        y: event.clientY - this.position.y
+      }
+      this.element.scrollTop = this.position.top - direction.y;
+      this.element.scrollLeft = this.position.left - direction.x;
+    }
+  }
+
+  @HostListener('mouseup')
+  handleMouseUp() {
+    this.position = undefined;
+  }
+
+  @HostListener('mousewheel', ['$event'])
+  handleScroll(event: WheelEvent) {
+    this.element.scrollBy({left: -event.deltaY, behavior: "smooth"})
   }
 
   constructor(private _el: ElementRef<HTMLElement>,
